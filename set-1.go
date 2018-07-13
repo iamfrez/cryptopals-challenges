7757ddc5d4fa17c15ffd12f3ccc4738e00a9deb7
Lautaro Colombo <lautaro@lautarocolombo.com>
2018-07-13 20:27:57 -0300

    Added challenge 2

--- set-1.go
+++ set-1.go
@@ -4,6 +4,7 @@ import (
 	"encoding/hex"
 	"encoding/base64"
 	"log"
+	"os"
 )
 
 func HexToBase64(s string) string {
@@ -12,4 +13,26 @@ func HexToBase64(s string) string {
 		log.Fatal(err)
 	}
 	return base64.StdEncoding.EncodeToString([]byte (dec))
+}
+
+func FixedXOR(buff1 string, buff2 string) string {
+	if len(buff1) == 0 || len(buff2) == 0 {
+		os.Exit(1)
+	}
+	decBuff1, err := hex.DecodeString(buff1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	decBuff2, err2 := hex.DecodeString(buff2)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	
+	a := len(decBuff1)
+	b := make([]byte, a)
+	for i := 0; i < a; i++ {
+		b[i] = decBuff1[i] ^ decBuff2[i]
+	}
+
+	return hex.EncodeToString(b)
 }
--- set-1_test.go
+++ set-1_test.go
@@ -6,7 +6,14 @@ import (
 
 func TestChallenge1(t *testing.T) {
 	result := HexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")	
-	if (result != "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t") {
+	if result != "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" {
+		t.Error("Incorrect string", result)
+	}
+}
+
+func TestChallenge2(t *testing.T) {
+	result := FixedXOR("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
+	if result != "746865206b696420646f6e277420706c6179" {
 		t.Error("Incorrect string", result)
 	}
 }
